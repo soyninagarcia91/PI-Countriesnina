0e896d055dc086ace61601faf2ddbec72f59d33c
samunu <micaolor@gmail.com>
2024-01-28 19:16:26 -0300

    back done

--- server/src/controllers/getTableCountries.js
+++ server/src/controllers/getTableCountries.js
@@ -1,16 +1,13 @@
-const {Country} = require ('../db')
-const {Activity} = require('../db')
+const { Country } = require("../db");
+const { Activity } = require("../db");
 
+const getTableCountries = async (req, res) => {
+  try {
+    const allCountries = await Country.findAll();
+    res.status(200).json(allCountries);
+  } catch (error) {
+    req.status(500);
+  }
+};
 
-const getTableCountries = async(req, res) => {
-    try {
-        const allCountries = await Country.findAll({
-            include:Activity
-        })
-        res.status(200).json(allCountries)
-    } catch (error) {
-        req.status(500)
-    }
-}
-
-module.exports = getTableCountries
+module.exports = getTableCountries;
